749478371bbdf3681d5881ee41510601d69678a1
Lulezer <150733591+Lulezer@users.noreply.github.com>
2025-01-14 15:20:49 -0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,40 @@
-#include "echlib.h"
+#include "echlib.h"  // Include raylib header
+#include <iostream>
 
-int main() {
-    MakeWindow(600, 600, "Hello World");
+int main(void)
+{
+    // Initialization
+    const int screenWidth = 800;
+    const int screenHeight = 450;
 
-    while (!WindowShouldClose()) {
+    // Load sound files
+    Sound fxWav = LoadSound(RESOURCES_PATH "song.wav");  // Load sound file (change extension if needed)
+
+    MakeWindow(screenWidth, screenHeight, "raylib [audio] example - sound loading and playing");
+
+    InitAudioDevice();  // Initialize audio device
+
+    // Set target FPS
+    SetTargetFps(60);
+
+    PlaySound(fxWav);
+
+
+    // Main game loop
+    while (!WindowShouldClose()) {  // Detect window close button or ESC key
+
+        // Draw
         StartDrawing();
 
-        ClearBackground(BLACK);
-        DrawTriangle(0.0f, 0.0f, 1.0f, 1.0f, RED);
+        ClearBackground(WHITE);
 
         EndDrawing();
     }
 
-    CloseWindow();
+    // De-initialization
+    UnloadSound(fxWav);  // Unload sound data
+    CloseAudioDevice();   // Close audio device
+    CloseWindow();        // Close window and OpenGL context
+
     return 0;
 }
